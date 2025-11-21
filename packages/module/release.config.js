4e95b566eac392b51814f5baf83ae61891162a29
Austin Sullivan <ausulliv@redhat.com>
2025-11-21 11:44:46 -0500

    chore(CI): update release config to use prerelease tag

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: [{ name: 'main', channel: 'prerelease' }],
+  branches: [{ name: 'main', channel: 'prerelease', prerelease: 'prerelease' }],
   analyzeCommits: {
     preset: 'angular'
   },
@@ -9,6 +9,6 @@ module.exports = {
     '@semantic-release/github',
     '@semantic-release/npm'
   ],
-  tagFormat: 'v${version}',
+  tagFormat: 'prerelease-v${version}',
   dryRun: true
 };
